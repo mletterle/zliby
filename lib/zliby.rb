900ae7532f56995d0371d49f29d2849b71ddf4b3
mletterle <mletterle@0b6f053c-499f-4317-8076-a2c858260318>
2008-03-16 02:31:44 +0000

    Fixed order of classes for rdoc parsing
    git-svn-id: svn+ssh://rubyforge.org/var/svn/zliby/trunk@19 0b6f053c-499f-4317-8076-a2c858260318

--- lib/zliby.rb
+++ lib/zliby.rb
@@ -11,105 +11,6 @@ MAX_WBITS = 15
 Z_DEFLATED = 8 
 
 
-class GzipFile
-	
-	def initialize
-		@input_buffer = []
-		@output_buffer = []
-		@out_pos = -1
-		@in_pos = -1
-	end
-	
-	def close
-	end
-	
-	class Error < Exception
-	end
-
-end
-
-class GzipReader < GzipFile
-	OSES = ['FAT filesystem', 
-			'Amiga', 
-			'VMS (or OpenVMS)', 
-			'Unix', 
-			'VM/CMS', 
-			'Atari TOS', 
-			'HPFS fileystem (OS/2, NT)', 
-			'Macintosh', 
-			'Z-System',
-			'CP/M',
-			'TOPS-20',
-			'NTFS filesystem (NT)',
-			'QDOS',
-			'Acorn RISCOS',
-			'unknown']
-	def initialize io
-		super()
-		@io = io
-		io.read.each_byte {|b| @input_buffer << b}
-		if @input_buffer[@in_pos+=1] != 0x1f || @input_buffer[@in_pos+=1] != 0x8b then raise Zlib::GzipFile::Error.new("not in gzip format") end
-		if @input_buffer[@in_pos+=1] != 0x08 then raise Zlib::GzipFile::Error.new("unknown compression method") end
-		flg = @input_buffer[@in_pos+=1]
-		@ftext = flg.isbitset? 0
-		@fhcrc = flg.isbitset? 1
-		@fextra = flg.isbitset? 2
-		@fname = flg.isbitset? 3
-		@fcomment = flg.isbitset? 4
-		@mtime = Time.at(@input_buffer[@in_pos+=1] | (@input_buffer[@in_pos+=1] << 8) | (@input_buffer[@in_pos+=1] << 16) | (@input_buffer[@in_pos+=1] << 24))
-		@xfl = @input_buffer[@in_pos+=1]
-		@os = OSES[@input_buffer[@in_pos+=1]]
-		if @fextra then 
-			@xlen = (@input_buffer[@in_pos+=1] | (@input_buffer[@in_pos+=1] << 8)) 
-			@xtra_field = []
-			@xlen.times {@xtra_field << @input_buffer[@in_pos+=1]}
-		end
-		if @fname then
-			@original_name = ""
-			until @original_name["\0"].nil? == false
-				@original_name.concat(@input_buffer[@in_pos+=1])
-			end
-			@original_name.chop!
-		end
-		if @fcomment then
-			@comment = ""
-			until @comment["\0"].nil? == false
-				@comment.concat(@input_buffer[@in_pos+=1])
-			end
-			@comment.chop!
-		end
-		if @fhcrc then
-			@header_crc = @input_buffer[@in_pos+=1] | (@input_buffer[@in_pos+=1] << 8)
-		end	
-		@contents = ""
-		until @in_pos == @input_buffer.length-1
-			@contents.concat(@input_buffer[@in_pos+=1])
-		end
-		 
-	end
-	
-	def read
-		#we do raw deflate, no headers
-		z = Zlib::Inflate.new -MAX_WBITS
-		z.inflate @contents
-	end
-	
-	def close
-	end
-
-	
-	class << self
-
-		def open filename
-			io = File.open filename
-			gz = self.new io
-			if block_given? then yield gz else gz end
-		end
-		
-	end
-end
-
-
 class ZStream
 	
 	def initialize
@@ -229,7 +130,7 @@ class ZStream
 		@bit_count -= need
 		val & ((1 << need) - 1)
 	end
-	
+public	
 end
 
 #== DEFLATE Decompression
@@ -519,6 +420,105 @@ end
 	end
 
 end
+
+
+class GzipFile
+	
+	def initialize
+		@input_buffer = []
+		@output_buffer = []
+		@out_pos = -1
+		@in_pos = -1
+	end
+	
+	def close
+	end
+	
+	class Error < Exception
+	end
+
+end
+
+class GzipReader < GzipFile
+	OSES = ['FAT filesystem', 
+			'Amiga', 
+			'VMS (or OpenVMS)', 
+			'Unix', 
+			'VM/CMS', 
+			'Atari TOS', 
+			'HPFS fileystem (OS/2, NT)', 
+			'Macintosh', 
+			'Z-System',
+			'CP/M',
+			'TOPS-20',
+			'NTFS filesystem (NT)',
+			'QDOS',
+			'Acorn RISCOS',
+			'unknown']
+	def initialize io
+		super()
+		@io = io
+		io.read.each_byte {|b| @input_buffer << b}
+		if @input_buffer[@in_pos+=1] != 0x1f || @input_buffer[@in_pos+=1] != 0x8b then raise Zlib::GzipFile::Error.new("not in gzip format") end
+		if @input_buffer[@in_pos+=1] != 0x08 then raise Zlib::GzipFile::Error.new("unknown compression method") end
+		flg = @input_buffer[@in_pos+=1]
+		@ftext = flg.isbitset? 0
+		@fhcrc = flg.isbitset? 1
+		@fextra = flg.isbitset? 2
+		@fname = flg.isbitset? 3
+		@fcomment = flg.isbitset? 4
+		@mtime = Time.at(@input_buffer[@in_pos+=1] | (@input_buffer[@in_pos+=1] << 8) | (@input_buffer[@in_pos+=1] << 16) | (@input_buffer[@in_pos+=1] << 24))
+		@xfl = @input_buffer[@in_pos+=1]
+		@os = OSES[@input_buffer[@in_pos+=1]]
+		if @fextra then 
+			@xlen = (@input_buffer[@in_pos+=1] | (@input_buffer[@in_pos+=1] << 8)) 
+			@xtra_field = []
+			@xlen.times {@xtra_field << @input_buffer[@in_pos+=1]}
+		end
+		if @fname then
+			@original_name = ""
+			until @original_name["\0"].nil? == false
+				@original_name.concat(@input_buffer[@in_pos+=1])
+			end
+			@original_name.chop!
+		end
+		if @fcomment then
+			@comment = ""
+			until @comment["\0"].nil? == false
+				@comment.concat(@input_buffer[@in_pos+=1])
+			end
+			@comment.chop!
+		end
+		if @fhcrc then
+			@header_crc = @input_buffer[@in_pos+=1] | (@input_buffer[@in_pos+=1] << 8)
+		end	
+		@contents = ""
+		until @in_pos == @input_buffer.length-1
+			@contents.concat(@input_buffer[@in_pos+=1])
+		end
+		 
+	end
+	
+	def read
+		#we do raw deflate, no headers
+		z = Zlib::Inflate.new -MAX_WBITS
+		z.inflate @contents
+	end
+	
+	def close
+	end
+
+	class << self
+
+		def open filename
+			io = File.open filename
+			gz = self.new io
+			if block_given? then yield gz else gz end
+		end
+		
+	end
+end
+
 
 #Generic Error
 class Error < Exception
