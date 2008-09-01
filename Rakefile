f95c31ddb4f999ee084e5bafbb65649e227d9fe2
mletterle <mletterle@0b6f053c-499f-4317-8076-a2c858260318>
2008-09-01 00:39:24 +0000

    Doc and Gem Generation
    git-svn-id: svn+ssh://rubyforge.org/var/svn/zliby/trunk@22 0b6f053c-499f-4317-8076-a2c858260318

--- Rakefile
+++ Rakefile
@@ -1,5 +1,31 @@
 require 'rubygems'
 require 'rake'
+require 'rake/gempackagetask'
+
+
+gem_spec = Gem::Specification.new do |gem|
+			gem.name = "Zliby"
+			gem.version = "0.0.5"
+			gem.author = "Michael Letterle"
+			gem.email = "theprokrammer@prokrams.com"
+			gem.homepage = "http://zliby.rubyforge.org/"
+			gem.platform = Gem::Platform::RUBY
+			gem.summary = "Compression Library In Pure Ruby"
+			gem.rubyforge_project = "zliby"
+			gem.files = FileList["{bin,lib}/**/*"].to_a
+			gem.require_path = "lib"
+			gem.test_files = FileList["{specs}/**/*spec.rb"].to_a
+			gem.has_rdoc = true
+			gem.extra_rdoc_files = ["README", "CHANGES", "LICENSE", "TODO"]
+		end
+	
+	Rake::GemPackageTask.new(gem_spec) do |package|
+		package.need_tar = true
+		package.need_zip = true
+	end
+
 task :doc do
 	sh 'rdoc README TODO CHANGES lib/zliby.rb --template jamis --title "Zliby -- All Zlib, Pure Ruby" --inline-source -U'
 end
+
+	
--- lib/zliby.rb
+++ lib/zliby.rb
@@ -1,5 +1,7 @@
+require 'stringio'
+
 module Zlib
-ZLIBY_VERSION = "0.0.2"
+ZLIBY_VERSION = "0.0.5"
 ZLIB_VERSION = "1.2.3"	
 VERSION = "0.6.0" #For compatibility with Ruby-core Zlib
 MAXBITS = 15
@@ -9,6 +11,42 @@ MAXCODES = (MAXLCODES+MAXDCODES)
 FIXLCODES = 288     
 MAX_WBITS = 15
 Z_DEFLATED = 8 
+
+def self.adler32 string="", adler=1
+	if adler > (2**128) - 1 then raise RangeError.new end 
+	accum1 = adler & 0xffff
+	accum2 = (adler >> 16) & 0xffff
+	
+	len = string.length
+	x = -1
+	while len > 0 
+		tlen = len > 5552 ? 5552 : len
+		len -= tlen
+		while tlen >0
+			x += 1
+			accum1 += string[x]
+			accum2 += accum1
+			tlen -= 1
+		end
+		accum1 %= 65521
+		accum2 %= 65521
+	end
+	accum2 << 16 | accum1
+end
+
+def self.crc_table
+	[0, 1996959894, 3993919788, 2567524794, 124634137, 1886057615, 3915621685, 2657392035, 249268274, 2044508324, 3772115230, 2547177864, 162941995, 2125561021, 3887607047, 2428444049, 498536548, 1789927666, 4089016648, 2227061214, 450548861, 1843258603, 4107580753, 2211677639, 325883990, 1684777152, 4251122042, 2321926636, 335633487, 1661365465, 4195302755, 2366115317, 997073096, 1281953886, 3579855332, 2724688242, 1006888145, 1258607687, 3524101629, 2768942443, 901097722, 1119000684, 3686517206, 2898065728, 853044451, 1172266101, 3705015759, 2882616665, 651767980, 1373503546, 3369554304, 3218104598, 565507253, 1454621731, 3485111705, 3099436303, 671266974, 1594198024, 3322730930, 2970347812, 795835527, 1483230225, 3244367275, 3060149565, 1994146192, 31158534, 2563907772, 4023717930, 1907459465, 112637215, 2680153253, 3904427059, 2013776290, 251722036, 2517215374, 3775830040, 2137656763, 141376813, 2439277719, 3865271297, 1802195444, 476864866, 2238001368, 4066508878, 1812370925, 453092731, 2181625025, 4111451223, 1706088902, 314042704, 2344532202, 4240017532, 1658658271, 366619977, 2362670323, 4224994405, 1303535960, 984961486, 2747007092, 3569037538, 1256170817, 1037604311, 2765210733, 3554079995, 1131014506, 879679996, 2909243462, 3663771856, 1141124467, 855842277, 2852801631, 3708648649, 1342533948, 654459306, 3188396048, 3373015174, 1466479909, 544179635, 3110523913, 3462522015, 1591671054, 702138776, 2966460450, 3352799412, 1504918807, 783551873, 3082640443, 3233442989, 3988292384, 2596254646, 62317068, 1957810842, 3939845945, 2647816111, 81470997, 1943803523, 3814918930, 2489596804, 225274430, 2053790376, 3826175755, 2466906013, 167816743, 2097651377, 4027552580, 2265490386, 503444072, 1762050814, 4150417245, 2154129355, 426522225, 1852507879, 4275313526, 2312317920, 282753626, 1742555852, 4189708143, 2394877945, 397917763, 1622183637, 3604390888, 2714866558, 953729732, 1340076626, 3518719985, 2797360999, 1068828381, 1219638859, 3624741850, 2936675148, 906185462, 1090812512, 3747672003, 2825379669, 829329135, 1181335161, 3412177804, 3160834842, 628085408, 1382605366, 3423369109, 3138078467, 570562233, 1426400815, 3317316542, 2998733608, 733239954, 1555261956, 3268935591, 3050360625, 752459403, 1541320221, 2607071920, 3965973030, 1969922972, 40735498, 2617837225, 3943577151, 1913087877, 83908371, 2512341634, 3803740692, 2075208622, 213261112, 2463272603, 3855990285, 2094854071, 198958881, 2262029012, 4057260610, 1759359992, 534414190, 2176718541, 4139329115, 1873836001, 414664567, 2282248934, 4279200368, 1711684554, 285281116, 2405801727, 4167216745, 1634467795, 376229701, 2685067896, 3608007406, 1308918612, 956543938, 2808555105, 3495958263, 1231636301, 1047427035, 2932959818, 3654703836, 1088359270, 936918000, 2847714899, 3736837829, 1202900863, 817233897, 3183342108, 3401237130, 1404277552, 615818150, 3134207493, 3453421203, 1423857449, 601450431, 3009837614, 3294710456, 1567103746, 711928724, 3020668471, 3272380065, 1510334235, 755167117]
+end
+
+def self.crc32 string="", crc=0
+	if crc > 2**128 - 1 then raise RangeError.new end
+	crc = crc ^ 0xffffffff
+	string.each_byte do |byte|
+		index = (crc ^ byte) & 0xff
+		crc = (crc >> 8) ^ crc_table[index] 
+	end
+	crc ^ 0xffffffff
+end
 
 
 class ZStream
@@ -78,18 +116,29 @@ class ZStream
 	
 	#True if stream is finished, otherwise False
 	def finished?
+		if @finished.nil? then
+			false
+		else
+			@finished
+		end
 	end
 	
 	#Flushes input buffer and returns the data therein.
 	def flush_next_in
 		@in_pos = @input_buffer.length
-		@input_buffer
+		@finished = true
+		ret = @input_buffer.pack("c*")
+		@input_buffer = []
+		ret
 	end
 	
 	#Flushes the output buffer and returns all the data
 	def flush_next_out
 		@out_pos = @output_buffer.length
-		@output_buffer
+		@finished = true
+		ret = @output_buffer.pack("c*")
+		@output_buffer = []
+		ret
 	end
 	
 	#Reset stream.  Input and Output buffers are reset.
@@ -136,6 +185,7 @@ end
 #== DEFLATE Decompression
 #Implements decompression of a RFC-1951[ftp://ftp.rfc-editor.org/in-notes/rfc1951.txt] compatible stream.
 class Inflate < ZStream
+
 	def initialize window_bits=MAX_WBITS
 		@w_bits = window_bits	
 		if @w_bits < 0 then 
@@ -143,16 +193,29 @@ class Inflate < ZStream
 			@w_bits *= -1 
 		end	
 		super()
+		@zstring = ""
 	end
 	
+	#Appends data to the input stream
+	def <<(string)
+		@zstring << string
+		inflate
+	end
+	
+	#Sets the inflate dictionary
+	def set_dictionary string
+		@dict = string
+		reset
+	end
+		
 	#==Example
 	# f = File.open "example.z"
 	# i = Inflate.new
 	# i.inflate f.read
-	def inflate zstring
-		
+	def inflate zstring=nil
+		@zstring = zstring unless zstring.nil?
 		#We can't use unpack, IronRuby doesn't have it yet.
-		zstring.each_byte {|b| @input_buffer << b}
+		@zstring.each_byte {|b| @input_buffer << b}
 		
 	 	unless @rawdeflate then
 
@@ -175,8 +238,12 @@ class Inflate < ZStream
 		preset_dictionary_flag = ((flags & 0x20) >> 0x05) == 1
 		compression_level = (flags & 0xC0) >> 0x06
 		
+		if preset_dictionary_flag and @dict.nil? then raise Zlib::NeedDict.new "Preset dictionary needed!" end
+		
 		#TODO:  Add Preset dictionary support
-		if preset_dictionary_flag then @in_pos += 4 end
+		if preset_dictionary_flag then 
+			@dict_crc = @input_buffer[@in_pos+=1] << 24 | @input_buffer[@in_pos+=1] << 16 | @input_buffer[@in_pos+=1] << 8 | @input_buffer[@in_pos+=1]
+		 end
 		
 		end
 		last_block = false
@@ -197,7 +264,8 @@ class Inflate < ZStream
 	#Finishes inflating and flushes the buffer
 	def finish
 		output = ""
-		@output_buffer.each {|c| output << c }
+		inflate unless @output_buffer.length > 0
+		@output_buffer.each {|c| output << c } 
 		super
 		output
 	end
@@ -368,8 +436,7 @@ class Inflate < ZStream
 	
 	def construct_tree huffman_tree, lengths, n_symbols
 		offs = []
-		
-		
+	
 		for len in (000..MAXBITS)
 			huffman_tree.count[len] = 0
 		end
@@ -377,7 +444,6 @@ class Inflate < ZStream
 		for symbol in (000..n_symbols)
 			huffman_tree.count[lengths[symbol]] += 1
 		end
-		
 		
 		if huffman_tree.count[0] == n_symbols then return 0 end
 		
@@ -455,7 +521,16 @@ class GzipReader < GzipFile
 			'QDOS',
 			'Acorn RISCOS',
 			'unknown']
+			
 	def initialize io
+		
+		#Add a helper method to check bits
+		::Fixnum.module_eval do
+			def isbitset? bit_to_check
+			if self & (2 ** bit_to_check)  == (2 ** bit_to_check) then true else false end
+		end
+		end
+
 		super()
 		@io = io
 		io.read.each_byte {|b| @input_buffer << b}
@@ -496,16 +571,28 @@ class GzipReader < GzipFile
 		until @in_pos == @input_buffer.length-1
 			@contents.concat(@input_buffer[@in_pos+=1])
 		end
+
+		#we do raw deflate, no headers
+		@zstream = Zlib::Inflate.new -MAX_WBITS
+		@inflated = StringIO.new(@zstream.inflate @contents)
 		 
 	end
-	
-	def read
-		#we do raw deflate, no headers
-		z = Zlib::Inflate.new -MAX_WBITS
-		z.inflate @contents
+		
+	def read length=nil
+		@inflated.read length
 	end
 	
-	def close
+	def eof?
+		@inflated.eof?
+	end
+	
+	def pos
+		@inflated.pos
+	end
+	
+	def rewind
+		@inflated.rewind
+		@io.seek 0, IO::SEEK_SET
 	end
 
 	class << self
@@ -534,9 +621,4 @@ end
 
 end
 
-#Add a helper method to check bits
-class Fixnum
-	def isbitset? bit_to_check
-		if self & (2 ** bit_to_check)  == (2 ** bit_to_check) then true else false end
-	end
-end
+
